b67da20c7cf87b00e1e96caf4d7f04049cc8419d
ahibis <nikitonvg@list.ru>
2022-08-20 18:55:47 +0300

    bug fix

--- eboba.js
+++ eboba.js
@@ -2,9 +2,9 @@
 const fs = require("fs/promises");
 const s1 = ["Д", "Б", "Х", "П"];
 const s2 = ["аб", "об", "уб", "ыб", "еб", "иб"];
-A = ["Пип", "Зелеб", "Еб", "Ебол", "Хеб", "Поп", "A", "E"];
+A = ["Пип", "Зелеб", "Еб", "Ебол", "Хеб", "Поп", "А", "E"];
 B = ["y", "ы", "e", "o", "боб"];
-C = ["лда", "га", "бa", "нгa", "ун", "ан", "ук", "он"];
+C = ["лда", "га", "ба", "нга", "ун", "ан", "ук", "он"];
 const words = [];
 A.forEach(a => B.forEach(b=>C.forEach(c => words.push(a + b + c))));
 s1.forEach(a=>s2.forEach(a1=>B.forEach(b=>C.forEach(c => words.push(a + a1 + b + c)))));

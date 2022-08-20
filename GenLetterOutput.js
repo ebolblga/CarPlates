f0ae46be21e6d1cf42295cdc9b3a046ed29e1688
ahibis <nikitonvg@list.ru>
2022-08-21 00:53:16 +0300

    genWord update

--- GenLetterOutput.js
+++ GenLetterOutput.js
@@ -8,8 +8,17 @@ console.log(words.length)
 
 // Доступные буквы: ABЕКМНОРСТУХ
 // Доступные цифры: 03469 (озчбд)
-const ans1 = words.filter(word=>/^[авекмнорстух]?[озчбд]{3}[авекмнорстух]{0,2}$/i.test(word))
-const ans2 = words.filter(word=>/^[авекмнорстух]?[озчбд]{0,2}$/i.test(word))
-const ans3 = words.filter(word=>/^[озчбд]{0,2}[авекмнорстух]{0,2}$/i.test(word))
+const ans1 = words
+  .filter(word=>/^[авекмнорстух]?[озчбд]{3}[авекмнорстух]{0,2}$/i.test(word))
+  .map(word=>/(^[авекмнорстух]?)([озчбд]{3})([авекмнорстух]{0,2})$/i.exec(word))
+  .map(([a,b,c,d])=>b.padStart(1,"*")+c+d.padEnd(2,"*"))
+const ans2 = words
+  .filter(word=>/^[авекмнорстух]?[озчбд]{0,2}$/i.test(word))
+  .map(word=>/^([авекмнорстух]?)([озчбд]{0,2})$/i.exec(word))
+  .map(([a,b,c])=>b.padStart(1,"*")+c.padEnd(5,"*"))
+const ans3 = words
+  .filter(word=>/^[озчбд]{0,2}[авекмнорстух]{0,2}$/i.test(word))
+  .map(word=>/^([озчбд]{0,2})([авекмнорстух]{0,2})$/i.exec(word))
+  .map(([a,b,c])=>b.padStart(4,"*")+c.padEnd(2,"*"))
 
 console.log(...ans1,...ans2,...ans3)
